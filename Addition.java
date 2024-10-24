dba3e3d205be16737471ef1a5a5f514ec6ba8298
TanjilaS <tanjila.shaikh@flynautsaas.com>
2024-10-24 12:05:41 +0530

    added chng

--- Addition.java
+++ Addition.java
@@ -2,7 +2,7 @@ public class Addition {
     public static void main(String[] args) {
         int a=10;
         int b =20;
-        int c = a*b;
-        System.out.println(c);
+        System.out.println("hi");
+        System.out.println();
     }
 }
